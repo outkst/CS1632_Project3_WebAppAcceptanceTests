f963ef4fabeeca74ea7aa93dd801ac2934516a47
Joe Meszar <mesz@outk.st>
2016-02-24 17:19:16 -0500

    Added a few (4) Selenium tests to test going to the main page, searching for installation guide, checking if login div container exists and then checking if login button exists.

--- CS1632_Project3_WebAppAcceptanceTests/src/AcceptanceTests.java
+++ CS1632_Project3_WebAppAcceptanceTests/src/AcceptanceTests.java
@@ -1,31 +1,12 @@
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- *
+ * CS 1632 - DELIVERABLE 3: Web Testing with BDD
+ * 
  * @author Joe Meszar (jwm54@pitt.edu) | Cyrus Ramavarapu (crr41@pitt.edu)
  */
-
 public class AcceptanceTests {
     
     public static void main(String[] args) {
-        // test code to mess around with selenium
-        WebDriver driver = new FirefoxDriver();
-        
-        // set a default wait time for DOM to load before doing anything
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        
-        // grab the arch wiki page
-        driver.get("https://wiki.archlinux.org/");
-        
-        // get the search box of the arch wiki page
-        WebElement searchBox = driver.findElement(By.id("searchInput"));
-        
-        // enter "Installation Guide" into the search box
-        searchBox.sendKeys("Installation Guide");
-        
-        // submit using the page's default submit button
-        searchBox.submit();
+        // main not needed for Selenium testing..
     }
 }
--- CS1632_Project3_WebAppAcceptanceTests/test/AcceptanceTestsTest.java
+++ CS1632_Project3_WebAppAcceptanceTests/test/AcceptanceTestsTest.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.concurrent.TimeUnit;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -15,41 +7,156 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- *
- * @author joe
+ * @author Joe Meszar (jwm54@pitt.edu) | Cyrus Ramavarapu (crr41@pitt.edu)
  */
 public class AcceptanceTestsTest {
-    
-    public AcceptanceTestsTest() {
-    }
-    
+
+    private static FirefoxDriver driver;
+    WebElement element;
+    private static final int implicitWaitTime = 2; //seconds
+
+    /**
+     * Before any test is run, start up the Browser instance and set a default wait time between page DOM loading.
+     */
     @BeforeClass
     public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        // create a new FireFox driver instance
+        driver = new FirefoxDriver();
+
+        // set a default time for Selenium to wait before
+        //      considering any operation a failure
+        driver.manage().timeouts().implicitlyWait(implicitWaitTime, TimeUnit.SECONDS);
     }
 
     /**
-     * Test of main method, of class AcceptanceTests.
+     * Close the browser instance after all of the testing is complete.
+     */
+    @AfterClass
+    public static void tearDownClass() {
+        driver.quit();
+    }
+
+    /**
+     * Make sure the home page is reachable and correct.
      */
     @Test
-    public void testMain() {
-        System.out.println("main Testing...");
-        String[] args = null;
-        AcceptanceTests.main(args);
-        
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype and not complete.");
+    public void visitMainPage() {
+        // announce test and build expectations
+        System.out.println("visitMainPage: Testing that we are on the main Archi Wiki page...");
+        String expectedHeaderText = "Main page";
+
+        // go to the main arch wiki page
+        driver.get("https://wiki.archlinux.org/");
+
+        // grab the header telling exactly what page is being shown
+        element = driver.findElement(By.id("firstHeading"));
+
+        // grab the header text
+        String actualHeaderText = element.getText();
+
+        // make sure the header text says 'Main page'
+        assertEquals(String.format("Page says '%s'; expected '%s'", actualHeaderText, expectedHeaderText), actualHeaderText, expectedHeaderText);
+
+        // test complete
+        System.out.println("\tvisitMainPage: Success!");
     }
-    
+
+    /**
+     * Use the search box and look for the "Installation Guide".
+     */
+    @Test
+    public void searchForInstallationGuide() {
+        // announce test and build expectations
+        System.out.println("searchForInstallationGuide: Testing search box for the 'Installation Guide'");
+        String expectedHeaderText = "Installation guide";
+
+        // go to the main arch wiki page
+        driver.get("https://wiki.archlinux.org/");
+
+        // get the search box of the arch wiki page
+        WebElement searchBox = driver.findElement(By.id("searchInput"));
+
+        // enter "Installation Guide" into the search box
+        searchBox.sendKeys("Installation Guide");
+
+        // submit using the page's default submit button
+        searchBox.submit();
+
+        // grab the header telling exactly what page is being shown
+        element = driver.findElement(By.id("firstHeading"));
+
+        // grab the header text
+        String actualHeaderText = element.getText();
+
+        // make sure the header text says 'Main page'
+        assertEquals(String.format("Page says '%s'; expected '%s'", actualHeaderText, expectedHeaderText), actualHeaderText, expectedHeaderText);
+
+        // test complete
+        System.out.println("\tsearchForInstallationGuide: Success!");
+    }
+
+    /**
+     * Make sure the div container exists for the account controls.
+     */
+    @Test
+    public void loginControlsDivExists() {
+        // announce test and build expectations
+        System.out.println("loginControlsDivExists: Testing that the div container for the account controls exists...");
+        String divClassName = "pBody";
+
+        // go to the main arch wiki page
+        driver.get("https://wiki.archlinux.org/");
+
+        // try to grab the div that contains the account controls
+        boolean divExists = !driver.findElements(By.className(divClassName)).isEmpty();
+        assertTrue(String.format("The div '%s' that holds the account controls cannot be found.", divClassName), divExists);
+
+        // test complete
+        System.out.println("\tloginControlsDivExists: Success!");
+    }
+
+    /**
+     * Make sure the login button exists, if the user is NOT logged-in currently. (First checks if the div container for
+     * the account controls exists).
+     */
+    @Test
+    public void loginButtonExists() {
+        // announce test and build expectations
+        System.out.println("loginButtonExists: Testing that the login button exists on the page...");
+        String loginButtonName = "pt-login";
+
+        // go to the main arch wiki page
+        driver.get("https://wiki.archlinux.org/");
+
+        // first make sure the div exists for the account controls
+        System.out.println("\tTesting the the div container exists that holds the login button...");
+        loginControlsDivExists();
+        System.out.println("\tThe div container exists, continuing test...");
+
+        // the div exists, so see if the login button exists
+        boolean loginControlExists = !driver.findElements(By.id(loginButtonName)).isEmpty();
+        assertTrue(String.format("The login control '%s' cannot be found.", loginButtonName), loginControlExists);
+
+        if (loginControlExists) {
+            assertTrue(loginControlExists);
+
+            // test complete
+            System.out.println("\tloginButtonExists: Success!");
+
+        } else {
+            // maybe the login control doesn't exist because we are LOGGED-IN!
+
+            // get Cyrus to log in and inspect page for the "LOG OUT" button ID name
+            String logoutButtonName = "pt-logout"; // assuming button id name is pt-logout
+
+            boolean logoutControlExists = !driver.findElements(By.id(logoutButtonName)).isEmpty();
+            if (logoutControlExists) {
+                // DO NOT FAIL THIS TEST, we are logged-in so the control should NOT exist
+                System.out.println("\tloginButtonExists: SKIPPING! (Currently logged-in so no login control will exist.)");
+
+            } else {
+                assertTrue(String.format("The login control '%s' cannot be found.", loginButtonName), loginControlExists);
+            }
+        }
+    }
 }
